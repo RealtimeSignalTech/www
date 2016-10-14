32a064f711d2ff80558a20a539bb4ff96e2884d0
realtimesignals <rajnish@realtimesig.com>
2016-10-14 15:02:15 +0530

    made modular changes

--- countdown/js/main.js
+++ countdown/js/main.js
@@ -1,13 +1,16 @@
 //countdown on offer bar
+
 $( document ).ready(function() {
-    $('#clock').countdown('2016/10/19', function(event){
-	$(this).html(event.strftime('%D days %H:%M:%S'));
-	});
+    var offer_date = '2016/10/19';
+    init_offer_time(offer_date);
 });
 
-
+function init_offer_time(offer_date) {
+    $('#clock').countdown(offer_date, function(event){
+        $(this).html(event.strftime('%D days %H:%M:%S'));
+	});
+}
 $(".close-offer-btn").on("click", function(event){
 	$(".offerBg").remove();
 	event.preventDefault();
 });
-
