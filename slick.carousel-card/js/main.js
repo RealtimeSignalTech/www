98367dd16c7ab9a5974af37c8592086073a2547d
Ashish Ranjan <arkumar404@gmail.com>
2016-10-18 12:43:16 +0530

    Added cards using slick carousel

---
+++ slick.carousel-card/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.test').slick({
+        infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		autoplay: 1000,
+		dots: true,
+		variableWidth: true,
+		rows: 1,
+		});
+      
+    });
