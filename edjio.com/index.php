f946d14e1a98e3d27373555a1cee3bbcd3dcf017
realtimesignals <rajnish@realtimesig.com>
2016-11-03 15:18:00 +0530

    vertical-tabs/index.html

---
+++ edjio.com/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+<title>edjio.com - engineering courses online</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js"></script>
+</head>
+<body class="bg-inverse">
+
+<style>
+.logo {
+    font-family:"Century Gothic";
+    font-size:5rem;
+    color:#fff;
+}
+a   { color:#fff; }
+a:hover   { color:green; }
+.gr { color:grey; }
+</style>
+
+<div class="container-fluid mt-2">
+    <div class="col-xs-12 col-md-6 offset-md-3" style="color:#fff;">
+        <div class="font-weight-bold card-block text-xs-center">
+            <b class="logo h1">
+                <span>ed</span><span>jio</span>
+            </b>
+            <div class="mt-2 gr">
+                Real Time Signals Tenchnologies India Private Limited<br>
+                <a href="www.realtimesig.com">www.realtimesig.com</a><br>
+                Follow us on
+                <a target="_null" href="https://www.facebook.com/RealTimeSignals"><i class="fa fa-facebook"></i>Facebook</a>
+                <a target="_null" href="https://www.linkedin.com/company/real-time-signals"><i class="fa fa-linkedin"></i>LinkedIn</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
