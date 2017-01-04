b3ab4c01a81acfa8d0d39aee9b235ee34161854e
realtimesignals <rajnish@realtimesig.com>
2017-01-04 09:31:42 +0530

    scss form and collapse navbar

---
+++ search-form/gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-ruby-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const webserver = require('gulp-webserver');
+const livereload = require('gulp-livereload');
+
+gulp.task('sass', () =>
+    sass('./*.scss', {sourcemap: false})
+        .on('error', sass.logError)
+        // for inline sourcemaps 
+        .pipe(sourcemaps.write())
+        // for file sourcemaps 
+        .pipe(sourcemaps.write('maps', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest('.'))
+        .pipe(livereload())
+);
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./**/*.scss', ['sass']);
+});
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            port: 8080,
+            open: true
+        }))
+});
+
+gulp.task('default', ['watch', 'sass', 'webserver']);
